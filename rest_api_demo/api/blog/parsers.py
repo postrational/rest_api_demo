b2f279c67702e6accf280dfce33b113a4485b929
Michal Karzyński <bitbucket@karzyn.com>
2016-06-19 12:06:19 +0200

    Parser cleanup

--- rest_api_demo/api/blog/parsers.py
+++ rest_api_demo/api/blog/parsers.py
@@ -1,10 +1,7 @@
 from flask_restplus import reqparse
 
 pagination_arguments = reqparse.RequestParser()
-pagination_arguments.add_argument('page', type=int, required=False, location='values')
-pagination_arguments.add_argument('per_page', type=int, required=False, location='values', choices=[2, 10, 20, 30, 40, 50])
-
-parser = reqparse.RequestParser()
-parser.add_argument('test', choices=('one', 'two'), location='path')
-# pagination_parser.add_argument('id', choices=('one', 'two'), help='Foo param: {error_msg}', location='path')
-# pagination_parser.add_argument('foo', choices=('one', 'two'), help='Foo param: {error_msg}')
+pagination_arguments.add_argument('page', type=int, required=False, default=1, help='Page number')
+pagination_arguments.add_argument('bool', type=bool, required=False, default=1, help='Page number')
+pagination_arguments.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
+                                  default=10, help='Results per page {error_msg}')
