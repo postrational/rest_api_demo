14432600d83979153a50a2d0fa80c4ab8c0a844b
Michal Karzyński <bitbucket@karzyn.com>
2016-06-19 21:24:00 +0200

    Remove status field

--- rest_api_demo/api/blog/serializers.py
+++ rest_api_demo/api/blog/serializers.py
@@ -5,7 +5,6 @@ blog_post = api.model('Blog post', {
     'id': fields.Integer(readOnly=True, description='The unique identifier of a blog post'),
     'title': fields.String(required=True, description='Article title'),
     'body': fields.String(required=True, description='Article content'),
-    'status': fields.String(required=True, enum=['DRAFT', 'PUBLISHED', 'DELETED']),
     'pub_date': fields.DateTime,
     'category_id': fields.Integer(attribute='category.id'),
     'category': fields.String(attribute='category.id'),
